584ee82142f9ee9691ce52871fe64c8128acdd86
Pandikiruthika <111949410+Pandikiruthika@users.noreply.github.com>
2023-09-29 00:30:12 -0700

    Add files via upload

---
+++ librarymanagement/models/Book.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        required: true
+    },
+    alternateTitle: {
+        type: String,
+        default: ""
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        default: ""
+    },
+    publisher: {
+        type: String,
+        default: ""
+    },
+    bookCountAvailable: {
+        type: Number,
+        required: true
+    },
+    isbn:{
+        type: String, 
+        required: true,
+    
+    },
+    bookStatus: {
+        type: String,
+        default: "Available"
+    },
+    categories: [{
+        type: mongoose.Types.ObjectId,
+        ref: "BookCategory"
+    }],
+    
+    transactions: [{
+        type: mongoose.Types.ObjectId,
+        ref: "BookTransaction"
+    }]
+},
+{
+    timestamps: true
+});
+
+export default mongoose.model("Book", BookSchema);
